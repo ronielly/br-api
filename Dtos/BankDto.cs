b12bb12fd824bc11fe2dd0ef954e381264ee2c40
ronielly <roniellysborges@gmail.com>
2024-12-04 17:07:57 -0300

    :pencil: rename BankResponse file to BankDto

---
+++ Dtos/BankDto.cs
@@ -0,0 +1,10 @@
+namespace BrApi.Dtos
+{
+    public class BankDto
+    {
+        public string Ispb { get; set; }
+        public string Name { get; set; }
+        public int Code { get; set; }
+        public string FullName { get; set; }
+    }
+}
